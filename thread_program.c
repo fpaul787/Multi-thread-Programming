289ab0c71b91ac6ab18035e2be4e3220271e82eb
Frantz Paul <fpaul787@gmail.com>
2021-03-18 00:02:31 -0400

    created thread function

--- thread_program.c
+++ thread_program.c
@@ -5,6 +5,8 @@
 /* Global variable: accessible to all threads */
 int thread_count = 0;
 
+void *Greeting(void* thread_num); /* Thread function */
+
 int main(int argc, char *argv[])
 {
   long thread; /* Use long in case of a 64-bit system */
@@ -29,4 +31,26 @@ int main(int argc, char *argv[])
   }
   thread_count = (int)val;
 
+  threads = malloc(thread_count*sizeof(pthread_t));
+
+  for (thread = 0; thread < thread_count; thread++)
+  {
+    // pthread_create(&thread_handles[thread], NULL, Hello, (void*)thread);
+    pthread_create(&threads[thread], NULL, Greeting, (void *)thread);
+  }
+
+  for (thread = 0; thread < thread_count; thread++)
+  {
+    pthread_join(threads[thread], NULL);
+  }
+
+  free(thread_handles);
+}
+
+void *Greeting(void* thread_num){
+  long my_thread_num = (long) thread_num;
+
+  printf("Hello from thread %ld of %d\n", my_thread_num, thread_count);
+
+  return NULL;
 }
